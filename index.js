59683b3cf861c5ee62249194715b02536850fe7c
Mercy2525 <mercywmuriithi.mm@gmail.com>
2023-07-14 14:11:21 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+// //Code your solutions in this file
+
+
+
+const people=[]
+
+ function writeCards(spy) {
+  for (let i = 0; i < spy.length; i++) {
+     people.push(`Thank you, ${spy[i]}, for the wonderful surprise gift!`); 
+  }
+return people;
+ }
+ 
+function countDown(value) {
+  while (value>=0) {
+    console.log(value)
+    value--
+  }
+  
+}
+
+countDown(10)
